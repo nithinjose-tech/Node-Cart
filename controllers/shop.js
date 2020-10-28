670a217a4b57cfe3620a6ccf88de6e46289bf0d7
nithinjose-tech <ninjos8@gmail.com>
2020-10-28 20:13:11 +0530

    first commit

---
+++ controllers/shop.js
@@ -0,0 +1,13 @@
+const Product = require("../models/product");
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render("shop/index", {
+        prods: rows,
+        pageTitle: "Shop",
+        path: "/",
+      });
+    })
+    .catch((err) => console.log(err));
+};
